fc478918703df235ebb6bcbca8f0920d2c1adff5
sunghyuki <sunghyuk1609@gmail.com>
2020-12-03 22:27:05 +0900

    add: 로그인 및 회원가입 xml 추가 그리고 액티비티 전환 구현

---
+++ pickt/app/src/main/java/com/example/pickt/UserInfo.java
@@ -0,0 +1,9 @@
+package com.example.pickt;
+
+public class UserInfo {
+    String name;
+    String email;
+    String password;
+    String nickName;
+    String phone;
+}
---
+++ pickt/app/src/main/java/com/example/pickt/UserList.java
@@ -0,0 +1,7 @@
+package com.example.pickt;
+
+import java.util.ArrayList;
+
+public class UserList {
+    ArrayList<UserInfo> users = new ArrayList<UserInfo>();
+}
